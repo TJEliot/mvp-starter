309d70e76a5fbee81897d9cf1936d9d2c6396f8a
Thomas <bgaesop@Thomass-MacBook.local>
2018-01-04 11:00:24 -0800

    added press release, mysql schema

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE test;
 
 USE test;
 
-CREATE TABLE items (
+CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
   username varchar(20) NOT NULL,
   balance int NOT NULL,
@@ -14,3 +14,32 @@ CREATE TABLE items (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
+INSERT INTO users (username, balance) VALUES ('Airyque', 100);
+INSERT INTO users (username, balance) VALUES ('Alex', 100);
+INSERT INTO users (username, balance) VALUES ('Allen', 100);
+INSERT INTO users (username, balance) VALUES ('Anna', 100);
+INSERT INTO users (username, balance) VALUES ('Calvin', 100);
+INSERT INTO users (username, balance) VALUES ('Daphne', 100);
+INSERT INTO users (username, balance) VALUES ('David', 100);
+INSERT INTO users (username, balance) VALUES ('Illya', 100);
+INSERT INTO users (username, balance) VALUES ('Kenneth', 100);
+INSERT INTO users (username, balance) VALUES ('Lam', 100);
+INSERT INTO users (username, balance) VALUES ('Lara', 100);
+INSERT INTO users (username, balance) VALUES ('Lory', 100);
+INSERT INTO users (username, balance) VALUES ('Matt', 100);
+INSERT INTO users (username, balance) VALUES ('Mishall', 100);
+INSERT INTO users (username, balance) VALUES ('Mishula', 100);
+INSERT INTO users (username, balance) VALUES ('Stephan', 100);
+INSERT INTO users (username, balance) VALUES ('Thomas', 100);
+INSERT INTO users (username, balance) VALUES ('Victor', 100);
+INSERT INTO users (username, balance) VALUES ('Angel', 100);
+INSERT INTO users (username, balance) VALUES ('Angela', 100);
+INSERT INTO users (username, balance) VALUES ('Archana', 100);
+INSERT INTO users (username, balance) VALUES ('Doug', 100);
+INSERT INTO users (username, balance) VALUES ('Dustin', 100);
+INSERT INTO users (username, balance) VALUES ('Gil', 100);
+INSERT INTO users (username, balance) VALUES ('Hayden', 100);
+INSERT INTO users (username, balance) VALUES ('Suhail', 100);
+INSERT INTO users (username, balance) VALUES ('Tiffany', 100);
+INSERT INTO users (username, balance) VALUES ( , 100);
